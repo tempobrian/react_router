5f64caa9b6c7b22b699f8f0021115ede4f2233de
Brian Pena <mr.penabrian@hotmail.com>
2024-01-10 17:07:34 -0500

    add redirect to sign-up if user is not logged in when visiting /profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, Navigate } from "react-router-dom";
 import { selectCurrentUser, selectIsLoggedIn } from "../features/session/sessionSlice";
 
 export default function Profile() {
@@ -8,6 +8,11 @@ export default function Profile() {
   const loggedIn = useSelector(selectIsLoggedIn);
 
   // use loggedIn to return a Navigate
+  if (!loggedIn) {
+    return (
+      <Navigate to='/sign-up' />
+    )
+  }
 
   return (
     <main>
