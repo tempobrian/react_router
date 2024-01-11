db767aadd755d0ba7a38712e6c81903bca9b0435
Brian Pena <mr.penabrian@hotmail.com>
2024-01-11 15:13:05 -0500

    add query parameters to articles and functionality to back/forward buttons

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -2,17 +2,14 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectArticles, filterArticles } from "../features/articles/articlesSlice";
 import Search from "./Search";
-import { Link } from "react-router-dom";
-
-// Import Link and useSearchParams from React Router
+import { Link, useSearchParams } from "react-router-dom";
 
 export default function Articles() {
   const articles = useSelector(selectArticles);
 
-  // Grab URLSearchParams object from useSearchParams hook
-
+  const [searchParams, setSearchParams] = useSearchParams();
   // Get the queryParams from object returned from useSearchParams and set to `title`
-  const title = '';
+  const title = searchParams.get('title');
 
   const filteredArticles = title ? filterArticles(title, articles) : Object.values(articles)
 
